958f07ce9e90fbf3219672ec72718c11ec1c9d63
Hustlecraftco <Hustlecraftco@gmail.com>
2025-12-30 14:37:27 -0500

    Fixed5

--- components/ui/Robot.tsx
+++ components/ui/Robot.tsx
@@ -6,7 +6,7 @@ import Spline from '@splinetool/react-spline';
 
 const RobotScene = memo(function RobotScene() {
   return (
-    <div className="w-full h-full pointer-events-none md:pointer-events-auto">
+    <div className="w-full h-full pointer-events-auto md:pointer-events-auto">
        <Spline scene="https://prod.spline.design/7u1sFSqNjkJO1NtH/scene.splinecode" />
     </div>
   );
