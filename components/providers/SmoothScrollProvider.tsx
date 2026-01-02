837562d3085f79d05442f0d182e7e57a2a72bc5c
Hustlecraftco <Hustlecraftco@gmail.com>
2026-01-02 01:27:10 -0500

    Fixed3

--- components/providers/SmoothScrollProvider.tsx
+++ components/providers/SmoothScrollProvider.tsx
@@ -15,12 +15,13 @@ export default function SmoothScrollProvider({ children }: SmoothScrollProviderP
         lerp: 0.1,
         duration: 1.5,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        direction: "vertical",
-        gestureDirection: "vertical",
-        smooth: true,
-        smoothTouch: false,
+        // --- UPDATED PROPERTIES BELOW ---
+        orientation: "vertical",        // changed from 'direction'
+        gestureOrientation: "vertical", // changed from 'gestureDirection'
+        smoothWheel: true,              // changed from 'smooth'
         touchMultiplier: 2,
         infinite: false,
+        // smoothTouch is removed (touch handling is native by default in v1)
       }}
     >
       {children}
